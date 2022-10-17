aecf4d3594d9f3271b062e6a3d1feaf33b2d13ba
Aurore Bral-Dupont <a.bral-dupont@stag.technofuturtic.education>
2022-10-17 09:45:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Verbe from './containers/verbe/verbe'
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './containers/NavBar.jsx/NavBar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Present from './pages/present/present';
 import Imparfait from './pages/imparfait/imparfait';
 import Futur from './pages/futur/futur';
-import Verbe from './containers/verbe/verbe';
-
-
 
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Conjuguer à l'indicatif.</h1>
+      <h1>La conjugaison</h1>
       <NavBar></NavBar>
-      <Verbe></Verbe>
+      <Verbe></Verbe> 
       <Routes>
-        <Route path="/Présent" element={<Present/>}/>
-        <Route path="/Imparfait" element={<Imparfait/>}/>
-        <Route path="/Futur" element={<Futur/>}/>
+        <Route path="/Présent"element={<Present/>}/>
+        <Route path="/Futur"element={<Futur/>}/>
+        <Route path="/Imparfait"element={<Imparfait/>}/>
       </Routes>
     </div>
   );
 }
 
 export default App;
+
+{/* <Routes>
+        <Route path="/Présent" element={<Present/>}/>
+        <Route path="/Imparfait" element={<Imparfait/>}/>
+        <Route path="/Futur" element={<Futur/>}/>
+      </Routes> */}
--- src/containers/verbe/verbe.jsx
+++ src/containers/verbe/verbe.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 const Verbe = () => {
+    
+
     return (
         <div>
-            
-        </div>
+            <h1>A quoi ça sert.</h1>
+        <p>La conjugaison des verbes sert à indiquer <strong>la</strong>  ou <strong>les</strong> personnes qui font l'action et le moment où se déroule l'action. La terminaison du verbe porte la marque de la personne et du temps. </p>
+        <p>On distingue 2 temps: Les temps simples et les temps composés. </p>
+       <h3>Les temps simples:</h3>
+       <p>Le verbe n'est composé que d'un seul mot. Parmi les temps simples on trouve:</p>
+       <ul>
+        <li>Le présent de l'indicatif.</li>
+        <li>L'imparfait de l'indicatif.</li>
+        <li>Le passé simple.</li>
+        <li>Le futur simple.</li>
+       </ul>
+       <h3>Les temps composés:</h3>
+        <p>Le verbe est composé de 2 mot. L'auxiliaire "être" ou "avoir" + le participe passé.</p>
+        Parmi les temps composé, on trouve:
+        <ul>
+            <li>Le passé composé de l'indicatif.</li>
+            <li>Le plus-que-parfait de l'indicatif.</li>
+        </ul>
+        <p>Nous allons nous concentrer sur les 3 premiers temps simple de l'indicatiif:</p>
+        <ul>
+            <li>Le présent, le futur et l'imparfait.</li>
+        </ul>
+<p>Voici un petit exercice qui va vous permettre de vous situer dans vos connaissances.</p>
+       
+        </div>  
     );
 };
 
